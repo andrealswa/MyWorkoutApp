7a0a7f002d40678aead96315b01a88101edb5c9e
andrealswa <swartz8@uwindsor.ca>
2019-11-28 10:13:26 -0500

    minor addition to training service

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -7,8 +7,15 @@ export class TrainingService {
     { id: "side-lunges", name: "Side Lunges", duration: 120, calories: 18 },
     { id: "burpees", name: "Burpees", duration: 60, calories: 8 }
   ];
+  private runningExercise: Exercise;
 
   getAvailableExercises() {
     return this.availableExercises.slice();
+  }
+
+  startExercise(selectedId: string) {
+    this.runningExercise = this.availableExercises.find(
+      ex => ex.id === selectedId
+    );
   }
 }
