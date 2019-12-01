c2d5309e239e6c981a62af00ef517c0079f1f87a
andrealswa <swartz8@uwindsor.ca>
2019-12-01 00:45:35 -0500

    optimized table with sorting, fixed bug

--- src/app/training/past-training/past-training.component.ts
+++ src/app/training/past-training/past-training.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from "@angular/core";
-import { MatTableDataSource } from "@angular/material";
+import { Component, OnInit, ViewChild, AfterViewInit } from "@angular/core";
+import { MatTableDataSource, MatSort } from "@angular/material";
 
 import { Exercise } from "../exercise.model";
 import { TrainingService } from "../training.service";
@@ -9,13 +9,19 @@ import { TrainingService } from "../training.service";
   templateUrl: "./past-training.component.html",
   styleUrls: ["./past-training.component.css"]
 })
-export class PastTrainingComponent implements OnInit {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
   displayedColumns = ["date", "name", "duration", "calories", "state"];
   dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort, { static: true }) sort: MatSort; //TAKES TWO ARGUMENTS!
 
   constructor(private trainingService: TrainingService) {}
 
   ngOnInit() {
     this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
   }
 }
